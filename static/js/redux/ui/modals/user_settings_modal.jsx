d1c9384466b4c090dede6c447c4d575d063cb533
James S. Wang <JJamesWWang@gmail.com>
2021-02-20 16:56:55 -0500

    Add the favorite number in preferences

--- static/js/redux/ui/modals/user_settings_modal.jsx
+++ static/js/redux/ui/modals/user_settings_modal.jsx
@@ -163,6 +163,10 @@ class UserSettingsModal extends React.Component {
     const preferences = !this.props.userInfo.FacebookSignedUp ? null : (
       <div>
         <div className="preference cf">
+          <h3>This is your favorite number: {
+            this.props.userInfo.favorite_num}</h3>
+        </div>
+        <div className="preference cf">
           <label className="switch switch-slide" htmlFor="social-courses-input">
             <input
               ref={(c) => { this.shareCourses = c; }} id="social-courses-input"
