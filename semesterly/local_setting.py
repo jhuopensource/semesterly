e0fc716ed28d6962ecf5e1ef9680b615bb93b215
Kyle Wu <kwu45@jh.edu>
2021-09-19 17:04:07 -0400

    Added local settings

---
+++ semesterly/local_setting.py
@@ -0,0 +1,11 @@
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
