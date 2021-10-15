c534a35325b72e7f0fec84a44f8a0ec40e0c5b2c
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2021-10-15 13:16:32 -0400

    remove local boron and run lint fix

--- static/js/redux/ui/modals/exploration_modal.jsx
+++ static/js/redux/ui/modals/exploration_modal.jsx
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import {WaveModal} from 'boron-15';
+import { WaveModal } from 'boron-15';
 import isEqual from 'lodash/isEqual';
 import classNames from 'classnames';
 import CourseModalBodyContainer from '../containers/modals/course_modal_body_container';
--- static/js/redux/ui/modals/preference_modal.jsx
+++ static/js/redux/ui/modals/preference_modal.jsx
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import {WaveModal} from 'boron-15';
+import { WaveModal } from 'boron-15';
 import SortMenuContainer from '../containers/sort_menu_container';
 
 class PreferenceModal extends React.Component {
