0672869bf9bc744639d3696b057ee910a4e8ad9b
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2025-04-20 15:33:12 -0400

    run as dry run for now

--- build/run_parser.sh
+++ build/run_parser.sh
@@ -6,7 +6,7 @@ cd /code
 # python3 manage.py ingest jhu --term Spring --years 2025;
 python3 manage.py ingest jhu --use-admin-settings;
 python3 manage.py digest jhu;
-
+python manage.py deleteinactiveuser --batch-size 100 --dry-run
 # Run all
 #/usr/bin/python manage.py ingest jhu
 #/usr/bin/python manage.py digest jhu
