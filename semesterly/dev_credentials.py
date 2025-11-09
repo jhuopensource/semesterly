37cfadc856b7300d1a18cc1093da30e50f52da29
root <root@DESKTOP-UKV0O21.localdomain>
2025-11-09 06:32:47 -0500

    creates foundation for backend changes for tracking feature use

--- semesterly/dev_credentials.py
+++ semesterly/dev_credentials.py
@@ -34,7 +34,7 @@ SECRETS = {
     # Not essential for testing, but can be filled in for advanced usage
     "GW_PASS": None,
     "GW_SECURITY_ANSWER": None,
-    "JHU_API_KEY": None,
+    "JHU_API_KEY": "pusT2oDl0kz1AuYaFTd1dQUJPA575WDR",
     "VANDY_USER": None,
     "VANDY_PASS": None,
     # Not essential for testing, but can be filled in for advanced mailing usage
